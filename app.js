5444407e230073a5d6f5c45e8ca41f15b738df85
sc-mark-17 <sebcamacho1703@gmail.com>
2019-04-26 08:12:03 -0500

    primer commit con express

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hola Mundo!'))
+
+app.listen(port, () => console.log(`Ejemplo app escuchando en puerto ${port}!`))
