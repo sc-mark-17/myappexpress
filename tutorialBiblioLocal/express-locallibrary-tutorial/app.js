09367661922dc8c3eea17f5b371b2e41953476b1
sc-mark-17 <sebcamacho1703@gmail.com>
2019-04-30 19:09:47 -0500

    routes creadas

--- tutorialBiblioLocal/express-locallibrary-tutorial/app.js
+++ tutorialBiblioLocal/express-locallibrary-tutorial/app.js
@@ -6,11 +6,12 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalogRouter = require('./routers/catalog'); //Import routes for "catalog" area of site
 
 var app = express();
 
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://sebcamacho1703:<maquina>@cluster0-n9or5.azure.mongodb.net/local_library?retryWrites=true';
+var mongoDB = 'mongodb+srv://sebcamacho1703:maquina@cluster0-n9or5.azure.mongodb.net/local_library?retryWrites=true';
 mongoose.connect(mongoDB, {useNewUrlParser: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter); // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
